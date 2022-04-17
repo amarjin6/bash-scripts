6761a8e09328ecc931823f59de5b4278c6eaee52
Alexander Marjin <amarjin6@gmail.com>
2022-04-17 20:01:36 +0300

    Add task 1

---
+++ task1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+errFile=$(basename "$0.err")
+if [ $# \< 1 ]
+then
+     echo "$(basename "$0"): Too few arguments" 1>"$errFile"
+     while IFS= read -r line; do echo "$line" >&2; done < "$errFile"
+     exit 1
+fi
+if [ $1 -gt "-1" ] && [ $1 -lt "1001" ]
+then
+     for pid in $(pgrep -U "$(id -nu "$1")")
+         do
+           echo "$pid $(ls /proc/$pid/fd/ 2>> $errFile | wc -l)" 2>> "$errFile"
+         done
+	while IFS= read -r line; do  echo "$(basename "$0"): $line" >&2; done < "$errFile"
+else
+     echo "$(basename "$0"): Invalid user uid: $1" 1>"$errFile"
+     while IFS= read -r line; do echo "$line" >&2; done < "$errFile"
+     exit 1
+fi
+exit 0
